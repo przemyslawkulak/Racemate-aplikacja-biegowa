d93632f4d34214b45963929c2514618ae2d767b7
przemyslawkulak <przemyslaw.kulak86@gmail.com>
2019-01-25 12:35:01 +0100

    Custom 404 and 500 update

--- Racemate/Racemate/settings.py
+++ Racemate/Racemate/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'y@xm2mr_e=5i**5szrbpv^#w336#(d2*5yrx-#h3z=_rrce#^h'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['localhost']
 
