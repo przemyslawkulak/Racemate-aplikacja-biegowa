0565362a8fcd229e642920629827f969b8a769b4
przemyslawkulak <przemyslaw.kulak86@gmail.com>
2019-03-04 11:55:41 +0100

    gitignore

---
+++ Racemate/local_settings.py
@@ -0,0 +1,14 @@
+
+# Database
+# https://docs.djangoproject.com/en/2.1/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'HOST': 'ec2-107-20-185-27.compute-1.amazonaws.com',
+        'NAME': 'dac9tu9hbdbl03',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'USER': 'psoudkoltxyrla',
+        'PASSWORD': 'caa891b4913e958999b10205f5cbe223e782963dd00a52d9f1809a65c5f66c01',
+        'PORT': 5432
+    }
+}
