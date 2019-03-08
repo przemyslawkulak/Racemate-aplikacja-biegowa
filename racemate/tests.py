6c0db3a471cb87b3902cc8fd7aebb15e002d3105
przemyslawkulak <przemyslaw.kulak86@gmail.com>
2019-03-08 19:25:23 +0100

    LoginTestCase class

--- racemate/tests.py
+++ racemate/tests.py
@@ -1,3 +1,19 @@
+import unittest
+
+
 from django.test import TestCase
 
+
 # Create your tests here.
+from faker import Faker
+
+from racemate.models import MyUser
+
+
+class LoginTestCase(unittest.TestCase):
+    def setUp(self):
+        """Populate test database with random data."""
+        self.faker = Faker("pl_PL")
+        for _ in range(5):
+            MyUser.objects.create(username=self.faker.name())
+
