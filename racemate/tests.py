93f9fe60d35380cff56f7a501374bac59b04d7a3
przemyslawkulak <przemyslaw.kulak86@gmail.com>
2019-03-10 16:27:09 +0100

    login / logout tests

--- racemate/tests.py
+++ racemate/tests.py
@@ -1,19 +1,56 @@
-import unittest
-
-
-from django.test import TestCase
-
-
 # Create your tests here.
-from faker import Faker
+from unittest import TestCase
+from django.test import Client
 
+from racemate.forms import LoginForm
 from racemate.models import MyUser
 
 
-class LoginTestCase(unittest.TestCase):
-    def setUp(self):
-        """Populate test database with random data."""
-        self.faker = Faker("pl_PL")
-        for _ in range(5):
-            MyUser.objects.create(username=self.faker.name())
+class LoginTestCase(TestCase):
 
+    def setUp(self):
+        self.client = Client()
+
+        self.credentials = {
+            'username': 'testuser',
+            'password': 'secret'}
+        self.user = MyUser.objects.create(**self.credentials)
+
+    def test_login(self):
+        # Get login page
+        response = self.client.get('/accounts/login/')
+
+        # Check response code
+        self.assertEquals(response.status_code, 200)
+
+        # Log the user in
+        self.client.login(username='XXX', password="XXX")
+
+        # Check response code
+        response = self.client.get('/accounts/login/')
+        self.assertEquals(response.status_code, 200)
+
+        self.assertTrue(self.user.is_authenticated)
+
+    def test_logout(self):
+        # Log in
+        self.client.login(username='XXX', password="XXX")
+
+    def tearDown(self):
+        self.credentials = {
+            'username': 'testuser',
+            'password': 'secret'}
+        MyUser.objects.filter(**self.credentials).delete()
+
+
+class User_Form_Test(TestCase):
+
+    # Valid Form Data
+    def test_UserForm_valid(self):
+        form = LoginForm(data={'login': "user3", 'password': "user"})
+        self.assertTrue(form.is_valid())
+
+    # Invalid Form Data
+    def test_UserForm_invalid(self):
+        form = LoginForm(data={'email': "", 'password': "mp", })
+        self.assertFalse(form.is_valid())
