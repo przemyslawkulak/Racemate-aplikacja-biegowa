aacb7bb61a9229c0f4d7dbd20ce3cb31e80fbfb9
przemyslawkulak <przemyslaw.kulak86@gmail.com>
2019-03-11 11:00:59 +0100

    correct/incorrect login tests

--- racemate/tests.py
+++ racemate/tests.py
@@ -6,17 +6,30 @@ from racemate.forms import LoginForm
 from racemate.models import MyUser
 
 
+class User_Form_Test(TestCase):
+
+    # Valid Form Data
+    def test_UserForm_valid(self):
+        form = LoginForm(data={'login': "testuser", 'password': "testpsswrd"})
+        self.assertTrue(form.is_valid())
+
+    # Invalid Form Data
+    def test_UserForm_invalid(self):
+        form = LoginForm(data={'login': "", 'password': "mp", })
+        self.assertFalse(form.is_valid())
+
+
 class LoginTestCase(TestCase):
 
     def setUp(self):
         self.client = Client()
 
         self.credentials = {
-            'username': 'testuser',
-            'password': 'secret'}
-        self.user = MyUser.objects.create(**self.credentials)
+            'username': 'user',
+            'password': 'testpsswrd'}
+        self.user = MyUser.objects.create_user(**self.credentials)
 
-    def test_login(self):
+    def test_correct_login(self):
         # Get login page
         response = self.client.get('/accounts/login/')
 
@@ -24,33 +37,57 @@ class LoginTestCase(TestCase):
         self.assertEquals(response.status_code, 200)
 
         # Log the user in
-        self.client.login(username='XXX', password="XXX")
+        self.client.login(**self.credentials)
 
         # Check response code
         response = self.client.get('/accounts/login/')
         self.assertEquals(response.status_code, 200)
+        print(response.context['user'].username)
 
-        self.assertTrue(self.user.is_authenticated)
+        # check if user is correct for credentials
+        self.assertEquals(response.context['user'].username, 'user')
 
-    def test_logout(self):
-        # Log in
-        self.client.login(username='XXX', password="XXX")
+        # check if user is not anonymous
+        self.assertFalse(self.user.is_anonymous)
+
+        response = self.client.get('/logout/')
+
+        self.assertIsNone(response.context)
+
+    def test_incorrect_login(self):
+        self.credentials = {
+            'username': 'user2',
+            'password': 'testpsswrd'}
+
+        # Log the incorect user in
+        self.client.login(**self.credentials)
+
+        response = self.client.get('/accounts/login/')
+        print(response.context)
+        # check if uer from cresentials is not logged
+        self.assertNotEquals(response.context['user'].username, 'user2')
+
 
     def tearDown(self):
+        MyUser.objects.filter(username='user').delete()
+
+
+class LogoutTestCase(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+
         self.credentials = {
-            'username': 'testuser',
-            'password': 'secret'}
-        MyUser.objects.filter(**self.credentials).delete()
+            'username': 'user',
+            'password': 'testpsswrd'}
+        self.user = MyUser.objects.create(**self.credentials)
+
+    def test_logout(self):
+        # Log out
+        response = self.client.get('/logout/')
+        # check response code
+        self.assertEquals(response.status_code, 302)
 
 
-class User_Form_Test(TestCase):
-
-    # Valid Form Data
-    def test_UserForm_valid(self):
-        form = LoginForm(data={'login': "user3", 'password': "user"})
-        self.assertTrue(form.is_valid())
-
-    # Invalid Form Data
-    def test_UserForm_invalid(self):
-        form = LoginForm(data={'email': "", 'password': "mp", })
-        self.assertFalse(form.is_valid())
+    def tearDown(self):
+        MyUser.objects.filter(username='user').delete()
