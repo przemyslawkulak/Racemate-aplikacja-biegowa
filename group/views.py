3c86502b1f55ea27aa17b611165490cb91724a69
przemyslawkulak <przemyslaw.kulak86@gmail.com>
2019-03-28 15:21:10 +0100

    Docstrings for app 'group'

--- group/views.py
+++ group/views.py
@@ -11,6 +11,9 @@ from racemate.models import RunningGroup, MyUser, Message, PastTraining
 
 
 class RunningGroupView(LoginRequiredMixin, View):
+    """
+    View with details of all members of the one running group
+    """
     def get(self, request, id):
         group = RunningGroup.objects.get(id=id)
         user = MyUser.objects.filter(members=group).order_by('id')
@@ -19,6 +22,12 @@ class RunningGroupView(LoginRequiredMixin, View):
 
 
 class ShowGroupsView(LoginRequiredMixin, View):
+    """
+    View of all groups in which user is member, with details:
+    - who is admin,
+    - members count
+    - date of creation
+    """
     def get(self, request):
         groups = []
         admin = []
@@ -28,19 +37,25 @@ class ShowGroupsView(LoginRequiredMixin, View):
             for j in admins:
                 admin = j.username
 
-            m = len(MyUser.objects.filter(members=i))
-            groups.append({"name": i.name, "admins": admin, "members": m, "date": i.date, "id": i.id})
+            members_number = len(MyUser.objects.filter(members=i))
+            groups.append({"name": i.name, "admins": admin, "members": members_number, "date": i.date, "id": i.id})
         return render(request, 'group/showgroups.html', {"groups": groups})
 
 
 class JoinGroupView(LoginRequiredMixin, View):
+    """
+    View of all groups in which user is NOT member, with details:
+    - who is admin,
+    - members count
+    - date of creation
+    - if user sent a request to join
+    """
     def get(self, request):
         groups = []
         admin = []
         group = RunningGroup.objects.all().exclude(members=request.user)
         join = Message.objects.filter(sender=request.user).exclude(groupjoin=None)
         group_join_id = {i.groupjoin.id for i in join}
-        print(group_join_id)
         for i in group:
             admins = MyUser.objects.filter(admins=i)
             for j in admins:
@@ -52,6 +67,9 @@ class JoinGroupView(LoginRequiredMixin, View):
 
 
 class JoinConfirmView(LoginRequiredMixin, View):
+    """
+    View to send a message to admin with request or showing text "you already request to group"
+    """
     def get(self, request, id):
         group = RunningGroup.objects.get(id=id)
         admins = MyUser.objects.filter(admins=group)
@@ -68,7 +86,7 @@ class JoinConfirmView(LoginRequiredMixin, View):
                         admin = j.username
                     m = len(MyUser.objects.filter(members=i))
                     groups.append({"name": i.name, "admins": admin, "members": m, "date": i.date, "id": i.id})
-                text = f'You already request to {group.name}'
+                text = f'You already requested to {group.name}'
                 return render(request, 'group/joingroup.html', {"groups": groups, "text": text})
             Message.objects.create(content=f"Request to join '{group.name}' ", sender=request.user, to=i,
                                    groupjoin=group)
@@ -76,6 +94,9 @@ class JoinConfirmView(LoginRequiredMixin, View):
 
 
 class AdminConfirmView(LoginRequiredMixin, View):
+    """
+    View to add another user to group
+    """
     def get(self, request, id, sender):
         g = RunningGroup.objects.get(id=id)
         m = MyUser.objects.get(id=sender)
@@ -85,6 +106,11 @@ class AdminConfirmView(LoginRequiredMixin, View):
 
 
 class MemberView(LoginRequiredMixin, View):
+    """
+    View to show detail of member of group
+    - 5 last trainings
+    - last messages with user
+    """
     def get(self, request, id):
         member = MyUser.objects.get(id=id)
         msg1 = Message.objects.filter(to=id).filter(sender=request.user).order_by('-date_sent')
@@ -105,6 +131,10 @@ class MemberView(LoginRequiredMixin, View):
 
 
 class CreateGroupView(LoginRequiredMixin, View):
+    """
+    View to insert name of group user'd like to create
+    POST - creation of group
+    """
     def get(self, request):
         form = CreateGroupForm
         return render(request, 'group/runninggroup_form.html', {'form': form})
@@ -112,7 +142,6 @@ class CreateGroupView(LoginRequiredMixin, View):
     def post(self, request):
         form = CreateGroupForm(request.POST)
         user = request.user
-        print(user)
         if form.is_valid():
             g = RunningGroup.objects.create(name=form['name'].value())
             g.admins.add(user)
@@ -121,6 +150,9 @@ class CreateGroupView(LoginRequiredMixin, View):
 
 
 class AdminView(LoginRequiredMixin, View):
+    """
+    View with users requested to join group which you are admin
+    """
     def get(self, request, id):
         g = RunningGroup.objects.get(id=id)
         m = Message.objects.filter(groupjoin=g)
@@ -133,6 +165,9 @@ class AdminView(LoginRequiredMixin, View):
 
 
 class ShowAdminView(LoginRequiredMixin, View):
+    """
+    View with list of group where user is admin
+    """
     def get(self, request):
         join = Message.objects.filter(to=request.user).exclude(groupjoin=None)
         groups = RunningGroup.objects.filter(admins=request.user.id)
