5e126d1fb6161ed5fc4b61db9258be38553b343b
przemyslawkulak <przemyslaw.kulak86@gmail.com>
2019-04-02 15:30:07 +0200

    tests for RunningGroupViewSet

--- api/tests.py
+++ api/tests.py
@@ -1,11 +1,11 @@
-from django.test import TestCase, Client
+from django.test import Client
 
 # Create your tests here.
-from django.urls import reverse
+
 from rest_framework import status
 from rest_framework.test import APITestCase
 
-from racemate.models import MyUser
+from racemate.models import MyUser, RunningGroup
 
 
 class TestMyUserViewSet(APITestCase):
@@ -36,8 +36,8 @@ class TestMyUserViewSet(APITestCase):
         Test for correct url for user details
         """
         self.client.login(**self.credentials)
-        response = self.client.get(f"/api/v1/users/1")
-        self.assertEqual(response.status_code, status.HTTP_301_MOVED_PERMANENTLY)
+        response = self.client.get(f"/api/v1/users/1", follow=True)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_create_user(self):
         """
@@ -86,6 +86,98 @@ class TestMyUserViewSet(APITestCase):
         self.client.login(**self.credentials)
         response = self.client.delete("/api/v1/users/", user, content_type='json')
         self.assertEqual(response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
+
+    def tearDown(self):
+        MyUser.objects.filter(username='user').delete()
+
+
+class RunningGroupViewSet(APITestCase):
+    """
+    TestCase for MyUserViewSet
+    """
+
+    def setUp(self):
+        self.client = Client()
+        self.credentials1 = {
+            'username': 'testuser',
+            'password': 'testpassword'}
+        user1 = MyUser.objects.create_user(**self.credentials1)
+
+        self.credentials2 = {
+            'username': 'testuser2',
+            'password': 'testpassword'}
+        user2 = MyUser.objects.create_user(**self.credentials2)
+
+        self.credentials3 = {
+            'name': 'testgroup'}
+        group = RunningGroup.objects.create(**self.credentials3)
+        group.admins.add(MyUser.objects.get(username='testuser'))
+
+    def test_correct_list_url(self):
+        """
+        Test for correct list url
+        """
+        self.client.login(**self.credentials1)
+        response = self.client.get("/api/v1/groups/")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_correct_url(self):
+        """
+        Test for correct url for user details
+        """
+        self.client.login(**self.credentials1)
+        group_id = RunningGroup.objects.get(name='testgroup').id
+
+        response = self.client.get(f"/api/v1/groups/{group_id}", follow=True)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    # def test_create_user(self):
+    #     """
+    #     Test for POST - method not allowed (forbidden when user is not logged)
+    #     """
+    #     response = self.client.post("/api/v1/users/", {'username': 'newuser', 'password': 'testpassword'},
+    #                                 content_type='json')
+    #     self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+    #     self.client.login(**self.credentials)
+
+    #     response = self.client.post("/api/v1/users/", {'username': 'newuser', 'password': 'testpassword'},
+    #                                 content_type='json')
+    #     self.assertEqual(response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
+    #
+    # def test_put_user(self):
+    #     """
+    #     Test for Put - method not allowed (forbidden when user is not logged)
+    #     """
+    #     response = self.client.put("/api/v1/users/", {'username': 'newuser2', 'password': 'testpassword'},
+    #                                content_type='json')
+    #     self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+    #     self.client.login(**self.credentials)
+    #     response = self.client.put("/api/v1/users/", {'username': 'newuser2', 'password': 'testpassword'},
+    #                                content_type='json')
+    #     self.assertEqual(response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
+    #
+    # def test_patch_user(self):
+    #     """
+    #     Test for Patch - method not allowed (forbidden when user is not logged)
+    #     """
+    #     response = self.client.patch("/api/v1/users/", {'username': 'newuser2', 'password': 'testpassword'},
+    #                                  content_type='json')
+    #     self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+    #     self.client.login(**self.credentials)
+    #     response = self.client.patch("/api/v1/users/", {'username': 'newuser2', 'password': 'testpassword'},
+    #                                  content_type='json')
+    #     self.assertEqual(response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
+    #
+    # def test_delete_user(self):
+    #     """
+    #     Test for Delete - method not allowed (forbidden when user is not logged)
+    #     """
+    #     user = MyUser.objects.get(username='testuser')
+    #     response = self.client.delete("/api/v1/users/", user, content_type='json')
+    #     self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+    #     self.client.login(**self.credentials)
+    #     response = self.client.delete("/api/v1/users/", user, content_type='json')
+    #     self.assertEqual(response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
 
     def tearDown(self):
         MyUser.objects.filter(username='user').delete()
